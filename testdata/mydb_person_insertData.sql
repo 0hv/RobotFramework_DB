ef49c7634ff6812d51ae141e542c0264c18683b2
0hv <98320184+0hv@users.noreply.github.com>
2022-04-03 19:13:07 +0200

    Delete testdata directory

--- testdata/mydb_person_insertData.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO mydb.person values(111,"Elza", "Karepetner");
-INSERT INTO mydb.person values(112,"Selma", "Tompir");
-INSERT INTO mydb.person values(113,"Jojo", "Kazeuka");
-INSERT INTO mydb.person values(114,"Tarek", "Loevimr");
-INSERT INTO mydb.person values(115,"Julia", "Talinga");
