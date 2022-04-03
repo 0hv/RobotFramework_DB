8f1713b2e51ff425adb9a65745cffcab594d6730
0hv <98320184+0hv@users.noreply.github.com>
2022-04-03 19:14:07 +0200

    Add files via upload

---
+++ testdata/mydb_person_insertData.sql
@@ -0,0 +1,5 @@
+INSERT INTO mydb.person values(111,"Elza", "Karepetner");
+INSERT INTO mydb.person values(112,"Selma", "Tompir");
+INSERT INTO mydb.person values(113,"Jojo", "Kazeuka");
+INSERT INTO mydb.person values(114,"Tarek", "Loevimr");
+INSERT INTO mydb.person values(115,"Julia", "Talinga");
